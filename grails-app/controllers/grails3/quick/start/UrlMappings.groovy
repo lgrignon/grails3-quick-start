e0c2f29f92924abf394441be5b3a1b9ce758a20f
Louis Grignon <louis.grignon@gmail.com>
2017-06-05 20:41:26 +0200

    initial add

---
+++ grails-app/controllers/grails3/quick/start/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package grails3.quick.start
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/grails3/quick/start/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails3.quick.start
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
